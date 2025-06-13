739f47fcb1ad0732f8624685a998c9aa6d7800b9
Bogdan Vasilinchuk <vasilinchuk.bogdan@gmail.com>
2025-06-13 20:48:57 +0300

    Initial Commit

---
+++ Tradernet/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tradernet
+//
+//  Created by Bogdan Vasilinchuk on 6/13/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
